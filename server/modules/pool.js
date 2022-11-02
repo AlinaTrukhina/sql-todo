68e2226e74615f8702a0589be78fb8696149b8a2
AlinaTrukhina <alina.trukhina@gmail.com>
2022-11-02 16:09:54 -0500

    update pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,23 @@
 const pg = require('pg');
 
-let pool = new pg.Pool({
-  database: "cloud-todo"
-});
+// set up environment variable
+// DATABASE_HOST = prime-alina-demo-db.cdaqcqcrctlc.us-east-2.rds.amazonaws.com 
 
+/*
+Database URL
+postgresql://username:password@databaseurl/database_name
+postgres://postgres:
+*/
+
+let pool;
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+  })
+}
+else {
+  pool = new pg.Pool({
+    database: "cloud-todo", // localhost
+  });
+}
 module.exports = pool;
